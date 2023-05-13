e419125c9647a7671882e5b44ce8058ab824b38f
mcunningto <104205708+mcunningto@users.noreply.github.com>
2023-05-13 17:38:49 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='imsholcal',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['pandas'],
+    include_package_data=True,
+    url='https://github.com/mcunningto/imsholcal',
+    license='MIT',
+    author='mcunningto',
+    author_email='mcunningto@gmail.com',
+    description='business day offset function with custom exchange based holiday calendar'
+)
